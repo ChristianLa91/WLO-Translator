ce0251593eb6e01c9edc4d03fcfaaabbf7e97b53
Mabukon <2196658+ChristianLa91@users.noreply.github.com>
2024-04-26 03:02:45 +0200

    Version 1.0.7
    Changed so that only Item.dat has a character limit.

---
+++ WLO Translator WPF/Item/ItemALoginData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WLO_Translator_WPF
+{
+    public class ItemALoginData
+    {
+        public ItemALoginData()
+        {
+        }
+
+        public string   Text1               { get; set; }
+        public string   Text2               { get; set; }
+        public int      ItemStartPosition   { get; set; }
+        public int      ItemEndPosition     { get; set; }
+        public int      Text1StartPosition  { get; set; }
+        public int      Text1EndPosition    { get; set; }
+        public int      Text2StartPosition  { get; set; }
+        public int      Text2EndPosition    { get; set; }
+    }
+}
